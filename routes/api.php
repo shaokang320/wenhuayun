830e9ed043949752de57e3d70c4e63e9f610f3e0
邵康 <shaokang_chen@163>
2019-12-02 17:35:14 +0800

    修改

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,9 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
 //微信路由，无需登录
 Route::group(['prefix' => 'weixin', 'namespace' => 'Weixin', 'middleware' => ['api']], function () {
 
