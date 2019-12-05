997e5ed2c950345f77e87918071935f8eccb7f7f
邵康 <shaokang_chen@163>
2019-12-05 11:21:14 +0800

    修改

--- app/Http/Controllers/Weixin/IndexController.php
+++ app/Http/Controllers/Weixin/IndexController.php
@@ -9,21 +9,21 @@ class IndexController extends CommonController
 
     public function index(Request $request)
     {
-//        if (isset($request->wenhua_id))
-//        {
-//            $res = model('Culture')
-//                ->join('enterprise','enterprise.id','=','culture.enterprise_id')
-//                ->where('culture.id',$request->wenhua_id)
-//                ->select('culture.*','enterprise.mobile','enterprise.longitude','enterprise.latitude','enterprise.end_time')
-//                ->first();
-//        }elseif(isset($request->id))
-//        {
-//            $res = model('Enterprise')->where('id',$request->id)->first();
-//        }
-//        if (strtotime(date($res->end_time))<time())
-//        {
-//            $res = '';
-//        }
+        if (isset($request->wenhua_id))
+        {
+            $res = model('Culture')
+                ->join('enterprise','enterprise.id','=','culture.enterprise_id')
+                ->where('culture.id',$request->wenhua_id)
+                ->select('culture.*','enterprise.mobile','enterprise.longitude','enterprise.latitude','enterprise.end_time')
+                ->first();
+        }elseif(isset($request->id))
+        {
+            $res = model('Enterprise')->where('id',$request->id)->first();
+        }
+        if (strtotime(date($res->end_time))<time())
+        {
+            $res = '';
+        }
         $data['info'] = '';
         $this->ApiJson('200',$data,'请求成功');
     }
