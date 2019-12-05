782c65fe9e0a42addf4b06b2134dcdc8f992daa2
邵康 <shaokang_chen@163>
2019-12-05 11:13:52 +0800

    修改

--- app/Http/Controllers/Weixin/IndexController.php
+++ app/Http/Controllers/Weixin/IndexController.php
@@ -24,7 +24,7 @@ class IndexController extends CommonController
         {
             $res = '';
         }
-        $data['info'] = $res;
+        $data['info'] = '';
         $this->ApiJson('200',$data,'请求成功');
     }
 
